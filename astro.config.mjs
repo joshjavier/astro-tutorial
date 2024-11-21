92d4d737653635c2956274fd61d224f69b1e77b3
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-21 15:21:34 +0800

    Add an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://astro-tutorial-20241121.netlify.app',
+});
